937a00c5e083929b1d01f1755d8dc16d48d5e85a
hr9116test <hr9116test@gmail.com>
2026-08-16 22:53:36 +0900

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,5 +1,5 @@
 // admin.js
-const GAS_URL = 'https://script.google.com/macros/s/AKfycbxwHodHyyDHI-zfjV-ANJl3SCQ7Di9756o9dFCgpqsnuSUR0w_p-6dkvL8rp_VCBiVo/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbxoag6ejgW-mYaRQwbD1ReTrC_eiZSLSxHUwLh4WSrjwZEgQtrqelgYaQ53gMGuOYKK/exec';
 
 document.addEventListener('DOMContentLoaded', () => {
   // State
