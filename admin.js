a7660577939e5035cf41c0c4e1431c5979eacf2c
hr9116test <hr9116test@gmail.com>
2026-08-31 16:34:50 +0900

    Add files via upload

--- admin.js
+++ admin.js
@@ -344,8 +344,7 @@ mockBookings = result.data.bookings.map(b => {
             const dTitle = document.getElementById('details-modal-title');
           if(dTitle) dTitle.innerText = isBlock ? 'ブロックの詳細' : '予約詳細';
           
-          const dNameLabel = document.querySelector('#detail-name-container .form-label');
-          if(dNameLabel) dNameLabel.style.display = isBlock ? 'none' : 'block';
+          
           
           const dPhoneContainer = document.getElementById('detail-phone-container');
           if(dPhoneContainer) {
